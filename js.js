b1d4f49467be492c72413db8b282177b8f9cb5d8
Developer20124 <alishahj371@gmail.com>
2024-06-06 02:00:58 -0700

    Add files via upload
    Code With Junaid is your go-to destination for insightful coding tutorials and programming tips. Join Junaid on a journey through the world of coding, where he breaks down complex concepts into easy-to-understand lessons. Whether you're a beginner or an experienced coder, find inspiration and enhance your skills with practical examples. Subscribe now for a coding adventure that empowers you to master the art of programming

---
+++ js.js
@@ -0,0 +1,7 @@
+var typed = new Typed((".skills"),{
+    strings : ["Frontend Developer","Youtuber","Ui designer"],
+    typeSpeed : 100,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
